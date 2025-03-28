5cbeedfd303eda9614810ff2be93effb5971e84b
GrammyGirl2012 <aira.2012.khan@gmail.com>
2025-03-28 14:55:40 +0500

    Add files via upload

---
+++ activity one.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+window = Tk()
+window.title("Codingal text editor")
+window.geometry("600x500")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+def open_file():
+    """Open a for editing."""
+    filepath = askopenfilename(
+      filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+if not filepath:
+  return
+txt_edit.delete(1.0, END)
+with open(filepath, "r") as input_file:
+   text = input_file.read()
+   txt_edit.inssert(END, text)
+   input_file.close()
+window.title(f"Codingal text editor -{filepath}")
+
+def save_file():
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+
+ if not filepath:
+        return
+with open(filepath, "w") as output_file:
+         text = txt_edit.get(1.0, END)
+        output_file.write(text)
+    window.title(f"Codingal's Text Editor - {filepath}")
+
+
+       
+
+
